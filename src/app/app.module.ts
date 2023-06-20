5abb49e1db88373f14a6f2d57d0542efa1fae9ff
Chad Lycos <lycosc@star.lcc.edu>
2023-06-20 13:24:58 -0400

    after tslib update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { JsonDropdownComponent } from './json-dropdown/json-dropdown.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { JsonDropdownComponent } from './json-dropdown/json-dropdown.component';
     JsonDropdownComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
