8892dd64db63726784d6884dff68d605d68c590e
Chad Lycos <lycosc@star.lcc.edu>
2023-06-26 15:06:29 -0400

    end day commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,10 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatCardModule } from '@angular/material/card';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SubjCrseDdComponent } from './subj-crse-dd/subj-crse-dd.component';
 
 @NgModule({
-  declarations: [AppComponent, DivDeptDDComponent],
+  declarations: [AppComponent, DivDeptDDComponent, SubjCrseDdComponent],
   imports: [
     BrowserModule,
     NgbModule,
---
+++ src/app/subj-crse-dd/subj-crse-dd.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subj-crse-dd',
+  templateUrl: './subj-crse-dd.component.html',
+  styleUrls: ['./subj-crse-dd.component.css']
+})
+export class SubjCrseDdComponent {}
